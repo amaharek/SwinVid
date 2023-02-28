149d536d3d953ff6123be1ad024fac0d9acd5fb8
Abdelrahman Maharek <abdelrahman.maharek@2k.com>
2023-02-28 00:58:20 +0000

    adding swin vis

---
+++ configs/vis/swinvis/selsa_faster_rcnn_swin_dc5_1x_imagenetvid.py
@@ -0,0 +1,60 @@
+_base_ = [
+    
+#     './faster_rcnn_swin_dc5.py',  #### candidate (2) 
+#     './faster_rcnn_r50_fpn.py',  # worked 
+    './faster_rcnn_swin_fpn.py',  # worked 
+
+#     './mask_rcnn_r50_fpn.py', #  doesn't work 
+#     './mask_rcnn_r50_dc5.py', # candidate (1)
+    
+    
+#     './mask_rcnn_swin_fpn.py',  #### candidate (2) 
+
+#      './mask_rcnn_swin_dc5.py',  #### assert len(featmap_sizes) == self.prior_generator.num_levels
+
+    './imagenet_vid_fgfa_style.py',
+    './default_runtime.py'
+]
+find_unused_parameters=True
+model = dict(
+    type='SELSA',
+    detector=dict(
+        roi_head=dict(
+            type='SelsaRoIHead',
+            bbox_head=dict(
+                type='SelsaBBoxHead',
+                num_shared_fcs=2,
+                aggregator=dict(
+                    type='SelsaAggregator',
+                    in_channels=1024,
+                    num_attention_blocks=16)))))
+
+# dataset settings
+data = dict(
+    val=dict(
+        ref_img_sampler=dict(
+            _delete_=True,
+            num_ref_imgs=14,
+            frame_range=[-7, 7],
+            method='test_with_adaptive_stride')),
+    test=dict(
+        ref_img_sampler=dict(
+            _delete_=True,
+            num_ref_imgs=14,
+            frame_range=[-7, 7],
+            method='test_with_adaptive_stride')))
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=1.0 / 3,
+    step=[2, 5])
+# runtime settings
+total_epochs = 7
+evaluation = dict(metric=['bbox'], interval=7)
